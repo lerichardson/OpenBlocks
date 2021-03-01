eb73b956d2be579c5db26908d041725312880b8d
dbwww <79813966+dbwww@users.noreply.github.com>
2021-03-01 06:42:18 -0800

    Shader functions in seperate file
    All of the OpenGL shader functions are all in a shaderInit file now!

--- src/engine.c
+++ src/engine.c
@@ -3,22 +3,13 @@
 
 #include <stdio.h>
 
+#include "models/fullblock.h"
 #include "shaders/terrain.h"
+#include "shaderInit.h"
 
 int* nptr = NULL;
 
-unsigned int CompileShader(unsigned int type, const char source[])
-{
-    unsigned int id = glCreateShader(type);
-    const char* src = source;
-    glShaderSource(id, 1, &src, nptr);
-    glCompileShader(id);
 
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-
-    return id;
-}
 
 int main(void)
 {
@@ -41,47 +32,7 @@ int main(void)
 
     glewInit();
 
-    float position[] = {
-
-        -0.5f, -0.5f, // 0
-         0.5f, -0.5f, // 1
-         0.5f,  0.5f, // 2
-        -0.5f,  0.5f  // 3
-
-    };
-
-    unsigned int indices[] = {
-
-        0, 1, 2,
-        2, 3, 0
-
-    };
-
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), position, GL_STATIC_DRAW);
-
-    unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-    unsigned int program = glCreateProgram();
-
-    glAttachShader(program, CompileShader(GL_VERTEX_SHADER, VertexShader));
-    glAttachShader(program, CompileShader(GL_FRAGMENT_SHADER, FragmentShader));
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(CompileShader(GL_VERTEX_SHADER, VertexShader));
-    glDeleteShader(CompileShader(GL_FRAGMENT_SHADER, FragmentShader));
-
-    glUseProgram(program);
-
+    unsigned int program = shaderInit();
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -100,10 +51,7 @@ int main(void)
         glfwPollEvents();
     }
 
-    glDeleteProgram(program);
-
-    glDeleteShader(CompileShader(GL_VERTEX_SHADER, VertexShader));
-    glDeleteShader(CompileShader(GL_FRAGMENT_SHADER, FragmentShader));
+    shaderCleanup(program);
 
     glfwTerminate();
     return 0;
---
+++ src/shaderInit.h
@@ -0,0 +1,8 @@
+#ifndef SHADERINIT_DOT_H
+#define	SHADERINIT_DOT_H
+
+unsigned int shaderInit();
+
+void shaderCleanup(unsigned int program);
+
+#endif // SHADERINIT_DOT_H
